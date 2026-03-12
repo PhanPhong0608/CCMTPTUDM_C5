880708a341057cf9c58c6b23819c6627c6e6269a
PhanPhong0608 <phanphong0608@gmail.com>
2026-03-12 13:15:47 +0700

    Add ABOUT US

---
+++ floral-shop/src/main/java/com/example/floralshop/FloralShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.floralshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloralShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloralShopApplication.class, args);
+	}
+
+}
